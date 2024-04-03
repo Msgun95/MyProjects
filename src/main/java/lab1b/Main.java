85bd7fac6085af1b0d890a2de2c35a1c6e09e27d
Msgun <msgunberhe24@gmail.com>
2024-04-03 10:05:56 -0500

    I added lab1B

---
+++ src/main/java/lab1b/Main.java
@@ -0,0 +1,92 @@
+package lab1b;
+
+import java.time.LocalDate;
+
+import lab1b.model.Employee;
+import lab1b.model.PensionPlan;
+
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
+
+public class Main {
+
+     static Employee[] employees = new Employee[]{
+             new Employee(1L, "Daniel", "Agar", LocalDate.of(2018, 01, 17), 842,74, 105945.50 ),
+             new Employee(2L, "Benard", "Shaw", LocalDate.of(2019, 04, 03), 842,74,  197750.00 ),
+             new Employee(3L, "Carly", "Agar", LocalDate.of(2019, 04, 03), 842,74,842842.00  ),
+             new Employee(4L, "Wesley", "Schneider", LocalDate.of(2019, 05, 02), 842,74,500.00  )
+
+
+             //     employees.get().setPensionPlan(new PensionPlan("EX1089", LocalDate.of(2018, 01, 17), 100.00)),
+    //     employees.get().setPensionPlan(new PensionPlan("SM2307", LocalDate.of(2019, 11, 04), 1555.50)),
+    //
+ };
+
+    static PensionPlan [] pensionPlans = new PensionPlan[]{
+        new PensionPlan("EX1089", LocalDate.of(2018, 01, 17), 100.00),
+       // new PensionPlan("", LocalDate.of(0, 0, 0), null),
+        new PensionPlan("SM2307", LocalDate.of(2019, 11, 04) ,555.50)
+ };
+
+
+
+    public static void main(String[] args) {
+
+
+        employees[0].setPensionplan(new PensionPlan("EX1089", LocalDate.of(2018, 01, 17), 100.00));
+       employees[2].setPensionplan(new PensionPlan("SM2307", LocalDate.of(2019, 11, 04), 555.50));
+
+
+        printEmployeesInJSON(employees);
+        printMonthlyUpcomingEnrollees();
+
+    }
+    private static void printEmployeesInJSON(Employee[] employees) {
+        if(employees != null) {
+            if(employees.length > 0) {
+                // Sort by product's name in ascending order
+                var sortedemployees = Arrays.stream(employees)
+                        .sorted(Comparator.comparing(Employee::getLastname).thenComparing(Employee::getYearlySalary).reversed())
+                        .toList();
+                // Print in JSON format
+              //  System.out.println("Printed in JSON Format");
+                System.out.println("[");
+//                sortedProducts.forEach(p -> System.out.println(p.toJSONString()));
+                long numemployees = sortedemployees.size();
+                for (int i = 0; i < numemployees; i++) {
+                    if((i + 1) < numemployees ) {
+                        System.out.printf("%s,\n", sortedemployees.get(i).JSONString());
+                    } else {
+                        System.out.println(sortedemployees.get(i).JSONString());
+                    }
+
+}
+}
+
+System.out.println("]");
+}
+}  private static void printMonthlyUpcomingEnrollees( ) {
+        LocalDate nextMonth = LocalDate.now().plusMonths(1);
+        LocalDate firstDayOfNextMonth = nextMonth.withDayOfMonth(1);
+        LocalDate lastDayOfNextMonth = nextMonth.withDayOfMonth(nextMonth.lengthOfMonth());
+
+        List<Employee> upcomingEnrollees = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getEmploymentDate().plusYears(5).isBefore(lastDayOfNextMonth) ||
+                    employee.getEmploymentDate().plusYears(5).isEqual(lastDayOfNextMonth)) {
+                if (employee.getPensionplan() == null) {
+                    upcomingEnrollees.add(employee);
+                }
+            }
+        }
+
+        printEmployeesInJSON(upcomingEnrollees.toArray(new Employee[0]));
+    }
+
+
+}
---
+++ src/main/java/lab1b/model/Employee.java
@@ -0,0 +1,106 @@
+package lab1b.model;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+private Long employeeId;
+private String firstname;
+private String lastname;
+private LocalDate employmentDate;
+private Double yearlySalary;
+private PensionPlan pensionplan = new PensionPlan();
+
+
+
+public Employee(long l, String string, String string2, LocalDate localDate, int i, double d) {
+}
+public Employee(Long employeeId, String firstname, String lastname, LocalDate employmentDate, int i, int j, Double yearlySalary) {
+    this.employeeId = employeeId;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.employmentDate = employmentDate;
+    this.yearlySalary = yearlySalary;
+}
+public Long getEmployeeId() {
+    return employeeId;
+}
+public void setEmployeeId(Long employeeId) {
+    this.employeeId = employeeId;
+}
+public String getFirstname() {
+    return firstname;
+}
+public void setFirstname(String firstname) {
+    this.firstname = firstname;
+}
+public String getLastname() {
+    return lastname;
+}
+public void setLastname(String lastname) {
+    this.lastname = lastname;
+}
+public LocalDate getEmploymentDate() {
+    return employmentDate;
+}
+public void setEmploymentDate(LocalDate employmentDate) {
+    this.employmentDate = employmentDate;
+}
+public Double getYearlySalary() {
+    return yearlySalary != null ? yearlySalary : 0.0;
+}
+public void setYearlySalary(Double yearlySalary) {
+    this.yearlySalary = yearlySalary;
+}
+
+
+// @Override
+// public String toString() {
+//     return "Employee [employeeId=" + employeeId + ", firstname=" + firstname + ", lastname=" + lastname
+//             + ", employmentDate=" + employmentDate + ", yearlySalary=" + yearlySalary + "]";
+// }
+
+
+
+
+public PensionPlan getPensionplan() {
+    return pensionplan;
+}
+public void setPensionplan(PensionPlan pensionplan) {
+    this.pensionplan = pensionplan;
+}
+
+
+//@Override
+//public String toString() {
+//    final StringBuilder sb = new StringBuilder("Employee{");
+//    sb.append("employeeId=").append(employeeId);
+//    sb.append(", firstname='").append(firstname).append('\'');
+//    sb.append(", lastname=").append(lastname);
+//    sb.append(", employmentDate=").append(employmentDate);
+//    sb.append(", yearlySalary=").append(yearlySalary);
+//    sb.append('}');
+//    return sb.toString();
+//}
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", employmentDate=" + employmentDate +
+                ", yearlySalary=" + yearlySalary +
+                ", pensionplan=" + pensionplan +
+                '}';
+    }
+
+    public String JSONString() {
+    return String.format("\t{ \"employeeId\":%d, \"firstName\":\"%s\", \"lastName\":\"%s\", \"employmentDate\":\"%s\", \"yearlySalary\":%,.2f }",
+            employeeId, firstname, lastname, employmentDate, yearlySalary);
+}
+
+
+    
+}
